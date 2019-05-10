850d193a0d384c3a10c3a74854de9fdaadbf558a
Alex-Pobeda <34069629+Alex-Pobeda@users.noreply.github.com>
2019-05-10 21:20:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const buyButton = document.body.querySelector('.container');
 const cart = document.body.querySelector('.cart');
 
 getData = () => {
-  fetch('data.json')
+  fetch('https://github.com/Alex-Pobeda/magnolia/blob/gh-pages/data.json')
     .then(response => {
       if (response.ok) return response.json();
       throw new Error('Error fetching data');
